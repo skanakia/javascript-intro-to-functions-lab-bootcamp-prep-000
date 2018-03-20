c8a388d66ca207ca0a9b6d385f9f40abbb0ec439
Sameer Kanakia <skanakia1@gmail.com>
2018-03-20 15:15:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
