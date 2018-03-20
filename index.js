4a4110cfd0ac8fa62719b42b7bd8c27f90f905f4
Sameer Kanakia <skanakia1@gmail.com>
2018-03-20 15:33:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,11 +21,9 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string.toLowerCase === string) {
     return "I can't hear you"
-  } else if (string === "I love you, Grandma.") {
-    return "I love you, too."
   } else if (string.toUpperCase === string) {
     return "YES INDEED!"
   } else {
-    return "Hi Grandma!"
+    return "I love you, too."
   }
 }
