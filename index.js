e97522645d5dfcb03037e0e30f55374f04f903a5
Sameer Kanakia <skanakia1@gmail.com>
2018-03-20 15:23:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,5 +21,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if string.toLowerCase === string {
     console.log("I can't hear you") 
-  } if else string === 
+  } if else string === "I love you, Grandma." {
+    console.log()
+  }
 }
