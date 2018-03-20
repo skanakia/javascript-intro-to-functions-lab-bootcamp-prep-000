3d9f4680ed52ab6b486d83fb26cffd65cddcdaa0
Sameer Kanakia <skanakia1@gmail.com>
2018-03-20 15:30:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,12 +20,12 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string.toLowerCase === string) {
-    console.log("I can't hear you") 
+    return "I can't hear you"
   } else if (string === "I love you, Grandma.") {
-    console.log("I love you, too.")
+    return "I love you, too."
   } else if (string.toUpperCase === string) {
-    console.log("YES INDEED!")
+    return "YES INDEED!"
   } else {
-    console.log("Hi Grandma!")
+    return "Hi Grandma!"
   }
 }
